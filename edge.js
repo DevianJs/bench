161e3b10be2b9c91b0de5f7a84a794d35234307b
DevianJs <164692955+DevianJs@users.noreply.github.com>
2024-04-27 02:13:55 -0600

    Add files via upload

---
+++ edge.js
@@ -0,0 +1,20 @@
+const CACHE = "static_v1";
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            if(!cacheNames.includes(CACHE))
+            caches.open(CACHE).then(cache => cache.addAll(["./", "./index.html", "./icon.svg", "./manifest.json"]));
+        })
+    );
+});
+self.addEventListener("fetch", (e) => {
+    e.respondWith((async () => {
+        const request = e.request.url.split("?").shift();
+        const cache = await caches.open(CACHE)
+        const response = await cache.match(request);
+        if(response) return response;
+        try {return fetch(request)}
+        catch (e) {return null}
+    })());
+});
